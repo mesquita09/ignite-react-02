aef3378b88ba6feeee04a39a4f0db2d8f7d5d4c8
Adelson <mesquita.adelson@gmail.com>
2021-03-16 20:05:33 -0300

    component summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Header } from "./components/header";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
 	return (
 		<>
 			<Header />
+			<Dashboard />
 			<GlobalStyle />
 		</>
 	);
