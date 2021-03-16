1b5d0c2997f7bf90144c7f5393e878ee60d7eec0
Adelson <mesquita.adelson@gmail.com>
2021-03-16 19:25:35 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+import { Header } from "./components/header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
 	return (
-		<div className='App'>
-			<h1>Hello</h1>
+		<>
+			<Header />
 			<GlobalStyle />
-		</div>
+		</>
 	);
 }
